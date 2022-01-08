447c9a6d85fbd6c2944f89444c6243c2cad53dbe
Dilshan <tdm.dev2@gmail.com>
2022-01-08 16:51:04 +0530

    numbers.test.js file completed

--- __tests__/numbers.test.js
+++ __tests__/numbers.test.js
@@ -10,3 +10,10 @@ test('two plus two', () => {
     expect(value).toBe(4);
     expect(value).toEqual(4);
 });
+
+test('adding decimal numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);
+    //expect(value).toEqual(0.3);
+    expect(value).toBeCloseTo(0.3);
+});
