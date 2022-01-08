5ea628f2828f534f16c9227d394b6d0c91faf84b
Dilshan <tdm.dev2@gmail.com>
2022-01-08 14:05:20 +0530

    numbers.test.js half completed

---
+++ __tests__/numbers.test.js
@@ -0,0 +1,12 @@
+//@ts-check
+
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(2);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+    
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
